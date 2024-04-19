15dd3adc79c8c1c01c6ad25caf82abe80cc45542
nikhil yadav <iamnikhilyadav27@gmail.com>
2024-04-19 14:57:01 +0530

    toolbox ui

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import Menu from "@/components/Menu";
+import Toolbox from "@/components/ToolBox";
 
 export default function Home() {
   return (
     <>
       <Menu />
+      <Toolbox />
     </>
   );
 }
